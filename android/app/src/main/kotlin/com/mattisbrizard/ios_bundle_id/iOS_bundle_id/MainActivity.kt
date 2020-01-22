fd786c7a7118212227ef74710d28df4151dd3010
MattisBrizard <mattisbrizard@icloud.com>
2020-01-22 13:37:02 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/mattisbrizard/ios_bundle_id/iOS_bundle_id/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mattisbrizard.ios_bundle_id.iOS_bundle_id
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
