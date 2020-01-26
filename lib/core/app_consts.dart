12f7fd59e2fecb92a5a1a6038a6784d4f16bc347
MattisBrizard <mattisbrizard@icloud.com>
2020-01-26 21:58:52 +0100

    Added consts

---
+++ lib/core/app_consts.dart
@@ -0,0 +1,8 @@
+/// The constants used in the app.
+abstract class AppConsts {
+  const AppConsts._();
+
+  /// The application searching url.
+  static const String appSearchUrl =
+      'https://itunes.apple.com/search?limit=10&media=software&term=';
+}
