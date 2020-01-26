7915c0d70b11196a73ffdfef0b022b6f0a88c7a7
MattisBrizard <mattisbrizard@icloud.com>
2020-01-26 21:59:28 +0100

    Run app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/material.dart';
+import 'package:iOS_bundle_id/modules/app/app.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'iOS bundle id',
-      home: Container(),
-    );
-  }
-}
+void main() => runApp(const App());
